607ee88193a319f5bedd6fd023139d38c24b22c6
Flavio Milan <flaviomilan@outlook.com>
2018-11-03 16:05:15 -0300

    iniciando

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from random import randint
 from debauto.remessa import Caixa, Config, Debito, Inclusao
 
 cfg = Config(
@@ -12,12 +13,65 @@ cfg = Config(
 )
 
 a = Caixa(cfg, [
-    Debito(1,1,1,1,1,1,1,1),
-    Debito(2,2,2,2,2,2,2,1),
-    Debito(3,3,3,3,3,3,3,1),
-    Debito(4,4,4,4,4,4,4,1),
-    Debito(5,5,5,5,5,5,5,1),
-    Debito(6,6,6,6,6,6,6,1),
+    Debito(
+        randint(1,9999),    # identificacao      25 posições
+        randint(1,9999),    # agência            04 posições
+        randint(1,9999),    # conta              13 posições
+        "01/01/1900",       # data vencimento    08 posições
+        200.00,             # valor              15 posições
+        "03",               # código da moeda    02 posições
+        "Exemplo 1",        # identificação co   59 posições
+        1),                 # 0 - Normal    1 - Cancelamento
+
+    Debito(
+        randint(1,9999),    # identificacao      25 posições
+        randint(1,9999),    # agência            04 posições
+        randint(1,9999),    # conta              13 posições
+        "01/01/1900",       # data vencimento    08 posições
+        200.00,             # valor              15 posições
+        "03",               # código da moeda    02 posições
+        "Exemplo 2",        # identificação co   59 posições
+        1),                 # 0 - Normal    1 - Cancelamento
+
+    Debito(
+        randint(1,9999),    # identificacao      25 posições
+        randint(1,9999),    # agência            04 posições
+        randint(1,9999),    # conta              13 posições
+        "01/01/1900",       # data vencimento    08 posições
+        200.00,             # valor              15 posições
+        "03",               # código da moeda    02 posições
+        "Exemplo 3",        # identificação co   59 posições
+        1),                 # 0 - Normal    1 - Cancelamento
+
+    Debito(
+        randint(1,9999),    # identificacao      25 posições
+        randint(1,9999),    # agência            04 posições
+        randint(1,9999),    # conta              13 posições
+        "01/01/1900",       # data vencimento    08 posições
+        200.00,             # valor              15 posições
+        "03",               # código da moeda    02 posições
+        "Exemplo 4",        # identificação co   59 posições
+        1),                 # 0 - Normal    1 - Cancelamento
+
+    Debito(
+        randint(1,9999),    # identificacao      25 posições
+        randint(1,9999),    # agência            04 posições
+        randint(1,9999),    # conta              13 posições
+        "01/01/1900",       # data vencimento    08 posições
+        200.00,             # valor              15 posições
+        "03",               # código da moeda    02 posições
+        "Exemplo 5",        # identificação co   59 posições
+        1),                 # 0 - Normal    1 - Cancelamento
+
+    Debito(
+        randint(1,9999),    # identificacao      25 posições
+        randint(1,9999),    # agência            04 posições
+        randint(1,9999),    # conta              13 posições
+        "01/01/1900",       # data vencimento    08 posições
+        200,             # valor              15 posições
+        "03",               # código da moeda    02 posições
+        "Exemplo 6",        # identificação co   59 posições
+        1),                 # 0 - Normal    1 - Cancelamento
 ])
 
 a.gerar_arquivo_txt()
